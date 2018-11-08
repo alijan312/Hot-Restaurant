79e9155cf8bdeff1f48b1f0900be7855813d8626
Alijan Shiraliyev <aj@Alijans-MacBook-Pro.local>
2018-11-07 20:18:07 -0600

    fixed conflicts

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,14 +2,14 @@ const path = require("path");
 
 module.exports = function(app){
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "./../public/view.html"));
+        res.sendFile(path.join(__dirname, "./../public/home.html"));
       });
       
       app.get("/make", function(req, res) {
         res.sendFile(path.join(__dirname, "./../public/make.html"));
       });
       
-      app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "./../public/home.html"));
+      app.get("/view", function(req, res) {
+        res.sendFile(path.join(__dirname, "./../public/view.html"));
       });
 };
